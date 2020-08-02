dbf85c1831b6aa1b4d238524635bb337656ed29d
ngng628 <e16-jahx@kure.kosen-ac.jp>
2020-08-02 19:00:26 +0900

    はじめてのコミット

---
+++ ImageShapingTool/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace ImageShapingTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ファイルToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.開くToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.heightSize = new System.Windows.Forms.ComboBox();
+            this.widthSize = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.閉じるCToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.終了XToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ファイルToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(866, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ファイルToolStripMenuItem
+            // 
+            this.ファイルToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.開くToolStripMenuItem,
+            this.閉じるCToolStripMenuItem,
+            this.終了XToolStripMenuItem});
+            this.ファイルToolStripMenuItem.Name = "ファイルToolStripMenuItem";
+            this.ファイルToolStripMenuItem.Size = new System.Drawing.Size(98, 29);
+            this.ファイルToolStripMenuItem.Text = "ファイル(&F)";
+            // 
+            // 開くToolStripMenuItem
+            // 
+            this.開くToolStripMenuItem.Name = "開くToolStripMenuItem";
+            this.開くToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.開くToolStripMenuItem.Size = new System.Drawing.Size(232, 34);
+            this.開くToolStripMenuItem.Text = "開く(&O)";
+            this.開くToolStripMenuItem.Click += new System.EventHandler(this.開くToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(12, 60);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(591, 378);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(632, 400);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 38);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "変換";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            ".png",
+            ".jpg",
+            ".gif",
+            ".tif",
+            ".bmp",
+            ".emf",
+            ".wmf"});
+            this.comboBox1.Location = new System.Drawing.Point(632, 60);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(222, 26);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(629, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "変換後の形式";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(629, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "変換後の大きさ";
+            // 
+            // heightSize
+            // 
+            this.heightSize.FormattingEnabled = true;
+            this.heightSize.Items.AddRange(new object[] {
+            "16",
+            "32",
+            "64",
+            "128",
+            "256",
+            "512",
+            "1024"});
+            this.heightSize.Location = new System.Drawing.Point(703, 177);
+            this.heightSize.Name = "heightSize";
+            this.heightSize.Size = new System.Drawing.Size(151, 26);
+            this.heightSize.TabIndex = 3;
+            this.heightSize.Text = "100";
+            this.heightSize.Leave += new System.EventHandler(this.heightSize_Leave);
+            // 
+            // widthSize
+            // 
+            this.widthSize.FormattingEnabled = true;
+            this.widthSize.Items.AddRange(new object[] {
+            "32",
+            "64",
+            "128",
+            "256",
+            "512",
+            "1024"});
+            this.widthSize.Location = new System.Drawing.Point(703, 209);
+            this.widthSize.Name = "widthSize";
+            this.widthSize.Size = new System.Drawing.Size(151, 26);
+            this.widthSize.TabIndex = 3;
+            this.widthSize.Text = "100";
+            this.widthSize.Leave += new System.EventHandler(this.widthSize_Leave);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(629, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "縦サイズ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(629, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 18);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "横サイズ";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(632, 357);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(167, 22);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "ファイル名を連番に";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 39);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(140, 18);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "読み込んだファイル";
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox3.Location = new System.Drawing.Point(632, 149);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(170, 22);
+            this.checkBox3.TabIndex = 5;
+            this.checkBox3.Text = "縦横比を1:1に保つ";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // 閉じるCToolStripMenuItem
+            // 
+            this.閉じるCToolStripMenuItem.Name = "閉じるCToolStripMenuItem";
+            this.閉じるCToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.閉じるCToolStripMenuItem.Text = "閉じる(&C)";
+            this.閉じるCToolStripMenuItem.Click += new System.EventHandler(this.閉じるCToolStripMenuItem_Click);
+            // 
+            // 終了XToolStripMenuItem
+            // 
+            this.終了XToolStripMenuItem.Name = "終了XToolStripMenuItem";
+            this.終了XToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.終了XToolStripMenuItem.Text = "終了(&X)";
+            this.終了XToolStripMenuItem.Click += new System.EventHandler(this.終了XToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(866, 450);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.widthSize);
+            this.Controls.Add(this.heightSize);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "画像フォーマット整えマン";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ファイルToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 開くToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox heightSize;
+        private System.Windows.Forms.ComboBox widthSize;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ToolStripMenuItem 閉じるCToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 終了XToolStripMenuItem;
+    }
+}
+
---
+++ ImageShapingTool/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageShapingTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void 開くToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true;
+            ofd.Filter = "画像ファイル (*.jpg;*png;*.gif;*.bmp;*.exig;*.tif;*.wmf;*.emf)|*.jpg;*png;*.gif;*.bmp;*.exig;*.tif;*.wmf;*.emf|All Files(*.*)|*.*";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string fileName in ofd.FileNames)
+                {
+                    textBox1.Text += fileName + Environment.NewLine;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string extension = comboBox1.Text;
+            if (extension == "")
+            {
+                MessageBox.Show("拡張子を設定してください。", 
+                    "エラー",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            var paths = textBox1.Text.Replace("\r\n", "\n").Split(new[] { '\n', '\r' });
+
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "変換先フォルダの指定";
+            string saveFolder = Environment.SpecialFolder.Desktop.ToString();
+            if (fbd.ShowDialog(this) == DialogResult.OK)
+            {
+                saveFolder = fbd.SelectedPath + "/";
+            }
+
+            int height = int.Parse(heightSize.Text);
+            int width = int.Parse(widthSize.Text);
+
+            int index = 1;
+            foreach (string path in paths)
+            {
+                if (path == "") continue;
+                Bitmap image = new Bitmap(Image.FromFile(path), width, height);
+                String filename = checkBox2.Checked
+                    ? String.Format("{0:D5}", index) + extension
+                    : Path.GetFileNameWithoutExtension(path) + extension;
+                image.Save(saveFolder + filename, StringToImageFormat(extension));
+                image.Dispose();
+                index++;
+            }
+        }
+
+        private ImageFormat StringToImageFormat(String format)
+        {
+            switch (format)
+            {
+                case ".png":
+                    return ImageFormat.Png;
+                case ".jpg":
+                    return ImageFormat.Jpeg;
+                case ".gif":
+                    return ImageFormat.Gif;
+                case ".tif":
+                    return ImageFormat.Tiff;
+                case ".bmp":
+                    return ImageFormat.Bmp;
+                case ".emf":
+                    return ImageFormat.Emf;
+                case ".wmf":
+                    return ImageFormat.Wmf;
+                default:
+                    return null;
+            }
+        }
+
+        private void widthSize_Leave(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked)
+            {
+                heightSize.Text = widthSize.Text;
+            }
+        }
+
+        private void heightSize_Leave(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked)
+            {
+                widthSize.Text = heightSize.Text;
+            }
+        }
+
+        private void 閉じるCToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void 終了XToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
